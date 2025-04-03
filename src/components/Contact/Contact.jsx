2c9998459bdc45dc0604d28dc2ae7b4d5b3847e3
Sh1wang1 <shiwangiverma2003@gmail.com>
2025-04-03 20:18:32 +0530

    Initial commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:shiwangiverma2003@gmail.com">Mail</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="www.linkedin.com/in/shiwangi-verma-97b1582a5
+           ">Linkedin</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Sh1wang1">Github</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
